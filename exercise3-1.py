2cc36e26cd5488acf881cf141155e842b60b8fdf
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 10:46:56 -0300

    Add files via upload

---
+++ exercise3-1.py
@@ -0,0 +1,4 @@
+n1 = int(input("Escreva um número:"))
+n2 = int(input("Escreva outro número:"))
+r = n1+n2
+print("A soma entre {} e {} é igual a: {}" .format(n1,n2,r))
---
+++ exercise3.py
@@ -0,0 +1,4 @@
+n1 = int(input("Escreva um número:"))
+n2 = int(input("Escreva outro número:"))
+r = n1+n2
+print("A soma de", n1, "e", n2,  "é igual a:", r)
